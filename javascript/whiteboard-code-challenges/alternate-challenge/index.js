92fa22afad73b43f46ad30c0d34cabd1c3b3c551
Donna Ada <125130284+donnaada@users.noreply.github.com>
2023-07-17 00:33:26 +1000

    cc36

--- javascript/whiteboard-code-challenges/alternate-challenge/index.js
+++ javascript/whiteboard-code-challenges/alternate-challenge/index.js
@@ -13,6 +13,24 @@ const reverseList = (list) => {
   list.head = prev;
 
   return list;
+};
+
+const segregate = (arr) => {
+  let results = [];
+  let odd = [];
+  let even = [];
+
+  for (let i = 0; i < arr.length; i++){
+    if(arr[i]%2){
+      even.push(arr[i]);
+    } else {
+      odd.push(arr[i]);
+    }
+  }
+
+  results = [...even, ...odd];
+  return results;
+
 }
 
-module.exports = { reverseList };
+module.exports = { reverseList, segregate };
